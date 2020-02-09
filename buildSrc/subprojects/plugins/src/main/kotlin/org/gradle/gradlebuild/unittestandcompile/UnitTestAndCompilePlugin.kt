10d0639006d756d31c34c70175328fe9d19aeaf3
Adam Murdoch <adam@gradle.com>
2020-02-10 10:28:48 +1100

    Use the public `JavaInstallation` API to query and validate the build and test JVM metadata, instead of using a bunch of internal APIs.

--- buildSrc/subprojects/plugins/src/main/kotlin/org/gradle/gradlebuild/unittestandcompile/UnitTestAndCompilePlugin.kt
+++ buildSrc/subprojects/plugins/src/main/kotlin/org/gradle/gradlebuild/unittestandcompile/UnitTestAndCompilePlugin.kt
@@ -115,6 +115,7 @@ class UnitTestAndCompilePlugin : Plugin<Project> {
     fun Project.configureCompile() {
         afterEvaluate {
             val jdkForCompilation = rootProject.buildJvms.compileJvm.get()
+            println("-> USING ${jdkForCompilation.vendorAndMajorVersion} FOR COMPILATION")
 
             tasks.withType<JavaCompile>().configureEach {
                 configureCompileTask(this, options, jdkForCompilation)
@@ -218,6 +219,8 @@ class UnitTestAndCompilePlugin : Plugin<Project> {
     private
     fun Test.configureJvmForTest() {
         val jvmForTest = project.buildJvms.testJvm.get()
+        println("-> USING ${jvmForTest.vendorAndMajorVersion} FOR TESTING")
+
         jvmArgumentProviders.add(createCiEnvironmentProvider(this))
         executable = jvmForTest.javaExecutable.absolutePath
         environment["JAVA_HOME"] = jvmForTest.javaHome.absolutePath
